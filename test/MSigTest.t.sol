f4a65e228f8c11805acd78c930512e2f526a1a9c
Jaseempk <jasimpk011@gmail.com>
2024-01-19 10:14:32 +0530

    moore💥

--- test/MSigTest.t.sol
+++ test/MSigTest.t.sol
@@ -24,10 +24,7 @@ contract MSigTest is Test {
         if(v<27){
             v+=27; //sometimes the value of v tend to be 0 or 1, to override that...
         }
-        bytes memory signature= abi.encodePacked(r,s,v);
-        require(signature.length == 65, "Invalid signature length");
-
-        return signature; //creating proper Ethereum signatures using three signature components
+        return abi.encodePacked(r,s,v); //creating proper Ethereum signatures using three signature components
     }
 
     function testInitialSetup() public {
@@ -142,5 +139,99 @@ contract MSigTest is Test {
         assertTrue(executed);
         vm.stopPrank();
     }
+    // Test revoking approval of a transaction
+    function testRevokeApproval() public {
+        testApproveTransaction(); // First approve a transaction
+
+        vm.prank(address(0xAAA)); 
+        multiSigWallet.revokeApproval(0);
+
+        (, , , , uint256 count) = multiSigWallet.transactions(0);
+        assertEq(count, 0);
+    }
+
+    // Test failure to revoke approval when not approved
+    function testFailToRevokeApprovalWhenNotApproved() public {
+        testSubmitTransaction(); // First submit a transaction
+        bytes4 customError=bytes4(keccak256("MSig__TransactionNotApproved()"));
+        vm.expectRevert(customError);
+        vm.prank(address(0xAAA)); 
+        multiSigWallet.revokeApproval(0);
+    }
+
+    // Test executing a transaction without enough approvals
+    function testFailExecuteTransactionWithoutEnoughApprovals() public {
+        testApproveTransaction(); // First approve a transaction
+        bytes4 customError=bytes4(keccak256("MSig__InsufficientNumApprovals()"));
+        vm.expectRevert(customError);
+        vm.prank(address(0xAAA)); 
+        multiSigWallet.executeTransaction(0, new bytes[](0)); // Attempt execution with only 1 approval
+    }
+
+    // Edge Case: Test adding owner and immediately executing a transaction
+    function testAddOwnerAndExecuteTransaction() public {
+        address newOwner = address(0xEEE);
+        vm.prank(address(0xDDD)); 
+        multiSigWallet.addOwner(newOwner);
+
+        // New owner submits a transaction
+        vm.prank(newOwner); 
+        multiSigWallet.submitTransaction(address(0x111), 1 ether, "");
+
+        // Existing owners approve the transaction
+        vm.prank(address(0xAAA)); 
+        multiSigWallet.approveTransaction(0);
+        vm.prank(address(0xBBB)); 
+        multiSigWallet.approveTransaction(0);
+        vm.prank(address(0xDDD));
+        multiSigWallet.approveTransaction(0);
+
+        // New owner executes the transaction
+        vm.prank(newOwner); 
+        uint8 txIndex=0;
+        bytes32  txHash=multiSigWallet.getTransactionHash(txIndex); //creating hash of the transaction with the help of transaction index
+
+        bytes[] memory signatures=new bytes[](3);
+
+        (uint8 v0,bytes32 r0,bytes32 s0)=vm.sign(uint160(address(0xBBB)),txHash); //simulating txHash signing
+        (uint8 v1,bytes32 r1,bytes32 s1)=vm.sign(uint160(address(0xAAA)),txHash);
+        (uint8 v2,bytes32 r2,bytes32 s2)=vm.sign(uint160(address(0xDDD)),txHash);
+
+        signatures[0]=toBytesSignature(v0,r0,s0); //creating signatures with three ethereum signature components
+        signatures[1]=toBytesSignature(v1,r1,s1);
+        signatures[2]=toBytesSignature(v2,r2,s2);
+
+        console.logBool(multiSigWallet.isOwner(address(0xAAA)));
+        console.logBool(multiSigWallet.isOwner(address(0xBBB)));
+        console.logBool(multiSigWallet.isOwner(address(0xDDD)));
+
+        console.logBool(multiSigWallet.approved(txIndex,address(0xAAA)));
+        console.logBool(multiSigWallet.approved(txIndex,address(0xBBB)));
+        console.logBool(multiSigWallet.approved(txIndex,address(0xDDD)));
+
+        console.logBytes(signatures[0]);
+        console.logBytes(signatures[1]);
+        console.logBytes(signatures[2]);
+
+        multiSigWallet.executeTransaction(txIndex, signatures); // Empty signatures for simplicity
+
+
+        (, , , bool executed, ) = multiSigWallet.transactions(txIndex);
+        assertTrue(executed);
+    }
+
+    // Failure Mode: Non-owner attempts to execute a transaction
+    function testFailExecuteTransactionByNonOwner() public {
+        testApproveTransaction(); // First approve a transaction
+
+        // Approve from another owner
+        vm.prank(address(0xBBB)); 
+        multiSigWallet.approveTransaction(0);
+        bytes4 customError=bytes4(keccak256("MSig__OnlyWalletOwnersCanAccess()"));
+        vm.expectRevert(customError);
+        vm.prank(address(0x111)); // Non-owner
+        multiSigWallet.executeTransaction(0, new bytes[](0)); 
+    }
+
 
 }
